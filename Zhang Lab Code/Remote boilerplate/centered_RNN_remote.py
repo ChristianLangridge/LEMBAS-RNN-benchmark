a596972056d78459a7d06de17e5004b7b0f9ae76
ChristianLangridge <chris.p.langridge@gmail.com>
2026-01-12 14:15:49 +0000

    Retried eval with centered data to compare the role of mean-centering on performance (PCC)

--- Zhang Lab Code/Remote boilerplate/centered_RNN_remote.py
+++ Zhang Lab Code/Remote boilerplate/centered_RNN_remote.py
@@ -5,8 +5,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 # reading in full data files
-gene_expression = pd.read_csv(('/Users/christianlangridge/Desktop/Zhang-Lab/Zhang Lab Data/Full data files/Geneexpression (full).tsv'), sep='\t', header=0)
-tf_expression = pd.read_csv(('/Users/christianlangridge/Desktop/Zhang-Lab/Zhang Lab Data/Full data files/TF(full).tsv'), sep='\t', header=0)
+gene_expression = pd.read_csv(('/home/christianl/Zhang-Lab/Zhang Lab Data/Full data files/Geneexpression (full).tsv'), sep='\t', header=0)
+tf_expression = pd.read_csv(('/home/christianl/Zhang-Lab/Zhang Lab Data/Full data files/TF(full).tsv'), sep='\t', header=0)
 
 # test-train splitting
 x = tf_expression
